3475ff20d00e52f1036a0d92e3efc4bbbedede5c
Dahlia Foo <dahliafoo@gmail.com>
2022-06-04 16:31:05 +1000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quizzical/', //This is the base url of your website
   plugins: [react()]
 })
